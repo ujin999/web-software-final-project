bb16a7c3e599b585568097e91c7ddd274e994a58
ujin999 <epahg789@gmail.com>
2025-05-18 01:56:06 +0900

    캘린더 항목 추가

--- admin-react/src/pages/test/Test.jsx
+++ admin-react/src/pages/test/Test.jsx
@@ -1,7 +1,9 @@
+import ManagementCalendar from  'components/Calendar/Calendar'
+
 export default function Test(){
   return(
       <>
-        <p>Test</p>
+        <ManagementCalendar></ManagementCalendar>
       </>
   )
 }
