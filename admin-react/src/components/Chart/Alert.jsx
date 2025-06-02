95109bb482e8cf5771261b74699a948ec6a8bf71
ujin999 <epahg789@gmail.com>
2025-06-02 14:59:20 +0900

    alert 파일 모듈화

--- admin-react/src/components/Chart/Alert.jsx
+++ admin-react/src/components/Chart/Alert.jsx
@@ -1,20 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import "./Alert.css";
 
-const Alert = () => {
-  const tasks = [
-    { project: "User Error", risk: "confirmed", endDate: "2025-09-05", done: "system" },
-    { project: "New User", risk: "unconfirmed", endDate: "2025-09-08", done: "user" },
-    { project: "System Error", risk: "unconfirmed", endDate: "2025-09-12", done: "system" },
-    { project: "Leave User", risk: "confirmed", endDate: "2025-09-14", done: "system" },
-    { project: "User LV.UP", risk: "confirmed", endDate: "2025-09-18", done: "user" },
-    ];
-
+const Alert = ({warnings}) => {
   return (
     <div className="alert-container">
       <div className="header">
         <span className="title">Alert</span>
-        <button className="report-btn">Report</button>
+        <Link to='/errors' className="report-btn">Report</Link>
       </div>
       <div className="task-labels">
         <span>Alert</span>
@@ -22,7 +15,7 @@ const Alert = () => {
         <span>Date</span>
         <span>Sender</span>
       </div>
-      {tasks.map((task, index) => (
+      {warnings.map((task, index) => (
         <div key={index} className="task-item">
           <span className="project-name">{task.project}</span>
           <span className={`risk ${task.risk.toLowerCase().replace(" ", "-")}`}>
